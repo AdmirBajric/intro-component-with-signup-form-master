372f657d8532aa1386c816e44b91db4f7ac38724
Admir Bajric <admir.bajric.dev@gmail.com>
2023-01-07 20:20:22 +0100

    Update main.css, main.js, responsive.css

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const form = document.querySelector(".form");
 const btn = document.querySelector(".btn");
 const inputs = document.querySelectorAll("input");
+const spans = document.querySelectorAll(".form span");
+
+spans.forEach((a) => {
+  a.style.display = "none";
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -13,13 +18,13 @@ form.addEventListener("submit", (e) => {
           /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         )
         .test(a.value)
-        ? (span.style.opacity = 0)
-        : (span.style.opacity = 1);
+        ? (span.style.display = "none")
+        : (span.style.display = "");
     } else {
       const span = document.querySelector(`.${a.id}`);
       v8n().empty().test(a.value)
-        ? (span.style.opacity = 1)
-        : (span.style.opacity = 0);
+        ? (span.style.display = "")
+        : (span.style.display = "none");
     }
   });
 });
