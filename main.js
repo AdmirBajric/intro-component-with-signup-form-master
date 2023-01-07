a6d2e1fb26c09c5d6349f1c2f7747cd7d7e0baf0
Admir Bajric <admir.bajric.dev@gmail.com>
2023-01-07 18:46:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ const inputs = document.querySelectorAll("input");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const emailRegEx =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   inputs.forEach((a) => {
     if (a.type === "email") {
       const span = document.querySelector(`.${a.id}`);
-      v8n().pattern(emailRegEx).test(a.value)
+      v8n()
+        .pattern(
+          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+        .test(a.value)
         ? (span.style.opacity = 0)
         : (span.style.opacity = 1);
     } else {
